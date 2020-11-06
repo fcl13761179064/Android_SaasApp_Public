11f82d289797b9547348349fb85493747bb138c8
吴友金 <wuyoujin@sunseaaiot.com>
2020-11-06 14:39:19 +0800

    【施工app_v0.8】场景-生效时间段  设置生效时间为15.50-16.00 保存时提示跨天

--- app/src/main/java/com/ayla/hotelsaas/ui/SceneSettingEnableTimeActivity.java
+++ app/src/main/java/com/ayla/hotelsaas/ui/SceneSettingEnableTimeActivity.java
@@ -75,12 +75,16 @@ public class SceneSettingEnableTimeActivity extends BaseMvpActivity {
     protected void appBarRightTvClicked() {
         super.appBarRightTvClicked();
         if (!enableTime.isAllDay()) {
-            if (!(enableTime.getStartHour() <= enableTime.getEndHour() && enableTime.getStartMinute() <= enableTime.getEndMinute())) {
-                CustomToast.makeText(this,"时间范围不能跨天",R.drawable.ic_toast_warming).show();
+            if (enableTime.getStartHour() > enableTime.getEndHour()) {
+                CustomToast.makeText(this, "时间范围不能跨天", R.drawable.ic_toast_warming).show();
                 return;
             }
-            if(enableTime.getEndHour() == 0 && enableTime.getEndMinute() == 0){
-                CustomToast.makeText(this,"时间范围不能跨天",R.drawable.ic_toast_warming).show();
+            if (enableTime.getStartHour() == enableTime.getEndHour() && enableTime.getStartMinute() > enableTime.getEndMinute()) {
+                CustomToast.makeText(this, "时间范围不能跨天", R.drawable.ic_toast_warming).show();
+                return;
+            }
+            if (enableTime.getEndHour() == 0 && enableTime.getEndMinute() == 0) {
+                CustomToast.makeText(this, "时间范围不能跨天", R.drawable.ic_toast_warming).show();
                 return;
             }
         }
