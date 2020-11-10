1d4b3b84360c5f24d3625cf7ad6b0ace1e53fb00
吴友金 <wuyoujin@sunseaaiot.com>
2020-11-10 16:40:43 +0800

    阶段 项目创建页面UI开发

---
+++ app/src/main/java/com/ayla/hotelsaas/ui/CreateProjectActivity.java
@@ -0,0 +1,27 @@
+package com.ayla.hotelsaas.ui;
+
+import com.ayla.hotelsaas.R;
+import com.ayla.hotelsaas.base.BaseMvpActivity;
+import com.ayla.hotelsaas.base.BasePresenter;
+
+public class CreateProjectActivity extends BaseMvpActivity {
+    @Override
+    protected BasePresenter initPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_create_project;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
