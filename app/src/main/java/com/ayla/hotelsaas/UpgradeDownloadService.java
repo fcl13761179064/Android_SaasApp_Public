71b20acbdbe5e816a6bf573491a3a0b90b80ced3
吴友金 <wuyoujin@sunseaaiot.com>
2020-12-16 18:28:26 +0800

    app自动升级下载安装包完成后，增加notification的点击事件

--- app/src/main/java/com/ayla/hotelsaas/UpgradeDownloadService.java
+++ app/src/main/java/com/ayla/hotelsaas/UpgradeDownloadService.java
@@ -3,6 +3,7 @@ package com.ayla.hotelsaas;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -14,6 +15,7 @@ import androidx.core.app.NotificationManagerCompat;
 
 import com.ayla.hotelsaas.ui.CustomToast;
 import com.blankj.utilcode.util.AppUtils;
+import com.blankj.utilcode.util.IntentUtils;
 
 public class UpgradeDownloadService extends JobIntentService {
 
@@ -28,6 +30,7 @@ public class UpgradeDownloadService extends JobIntentService {
         String url = intent.getStringExtra("url");
         String title = intent.getStringExtra("title");
         String saveDir = "upgrade";
+
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
 
@@ -44,21 +47,32 @@ public class UpgradeDownloadService extends JobIntentService {
         builder.setSmallIcon(R.mipmap.ic_launcher).setContentTitle(title).setOngoing(true);
 
         new DownloadUtil().download(getApplicationContext(), url, saveDir, new DownloadUtil.OnDownloadListener() {
+
+            int currentProgress;
+
             @Override
             public void onDownloadSuccess(String path) {
                 isWorking = false;
                 Log.d(TAG, "onDownloadSuccess: " + path);
-                notificationManagerCompat.cancel(notifyId);
-
+                Intent installAppIntent = IntentUtils.getInstallAppIntent(path);
+                Notification build = builder.setContentText("下载完成,点击安装")
+                        .setContentIntent(PendingIntent.getActivity(getApplicationContext(), 0, installAppIntent, PendingIntent.FLAG_CANCEL_CURRENT))
+                        .setAutoCancel(true)
+                        .build();
+                notificationManagerCompat.notify(notifyId, build);
                 AppUtils.installApp(path);
             }
 
             @Override
             public void onDownloading(int progress) {
+                if (currentProgress == progress) {
+                    return;
+                }
                 isWorking = true;
                 Log.d(TAG, "onDownloading: " + progress);
-                Notification notification = builder.setProgress(100, progress, false).build();
+                Notification notification = builder.setProgress(100, progress, false).setContentText("已完成:" + progress + "%").build();
                 notificationManagerCompat.notify(notifyId, notification);
+                currentProgress = progress;
             }
 
             @Override
