a526acdee58ee056c1e3bac847c83fbd12da35a7
chunlei.fan <chunlei.fan@aylaasia.com>
2021-05-18 10:20:27 +0800

    ap配网

--- app/src/main/java/com/ayla/hotelsaas/ui/A2GatewayAddStatusActivity.java
+++ app/src/main/java/com/ayla/hotelsaas/ui/A2GatewayAddStatusActivity.java
@@ -49,14 +49,12 @@ public class A2GatewayAddStatusActivity extends BaseMvpActivity<DeviceAddGuideVi
     ImageView imageView;
     @BindView(R.id.tv_content)
     TextView textView;
-    @BindView(R.id.cb)
-    AppCompatCheckBox checkBox;
     @BindView(R.id.bt)
     Button button;
     @BindView(R.id.appBar)
     AppBar appBar;
 
-    private static final int LiNE_NET= 0;
+    private static final int LiNE_NET = 0;
     private static final int AP_NET = 1;
     private Bundle addInfo;
 
@@ -76,7 +74,7 @@ public class A2GatewayAddStatusActivity extends BaseMvpActivity<DeviceAddGuideVi
 
     @Override
     protected int getLayoutId() {
-        return R.layout.activity_a2_gataway_add_guide;
+        return R.layout.activity_a2_gataway_add_status;
     }
 
     @Override
@@ -86,82 +84,12 @@ public class A2GatewayAddStatusActivity extends BaseMvpActivity<DeviceAddGuideVi
 
     @Override
     protected void initListener() {
-        View viewById = appBar.findViewById(R.id.iv_right);
-        View tv_right = appBar.findViewById(R.id.tv_right);
-        viewById.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                menuClick(v);
-            }
-        });
-        tv_right.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                menuClick(v);
-            }
-        });
     }
 
-
-
-    //菜单按钮onClick事件
-    public void menuClick(View view) {
-        final List<String> items = new ArrayList<>();
-        items.add("网线配网");
-        items.add("AP 配网");
-        String ap_choose = SharePreferenceUtils.getString(getContext(), Constance.AP_NET_SELECT, "1");
-        final ApNetpopupWindowUtil popupWindow = new ApNetpopupWindowUtil(this ,items,ap_choose);
-        popupWindow.setItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                popupWindow.dismiss();
-                switch ((int) id) {
-                    case LiNE_NET:
-                        SharePreferenceUtils.saveString(getContext(), Constance.AP_NET_SELECT, "1");
-
-                        break;
-                    case AP_NET:
-                        SharePreferenceUtils.saveString(getContext(), Constance.AP_NET_SELECT, "2");
-                        break;
-                }
-
-            }
-        });
-        popupWindow.setOff(-30,0);
-        //根据后面的数字 手动调节窗口的宽度
-        popupWindow.show(view, 300);
-    }
 
     private void handleJump() {
         Intent mainActivity = new Intent(this, ScanActivity.class);
         startActivityForResult(mainActivity, REQUEST_CODE_FOR_DSN_SCAN);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if ((requestCode == REQUEST_CODE_FOR_DSN_SCAN || requestCode == REQUEST_CODE_FOR_DSN_INPUT) && resultCode == RESULT_OK) {//获取到了DSN
-            if (data != null) {
-                String deviceId = data.getStringExtra("result").trim();
-                if (!TextUtils.isEmpty(deviceId)) {
-                    if (deviceId.startsWith("Lark_DSN:") && deviceId.endsWith("##")) {
-                        deviceId = deviceId.substring(9, deviceId.length() - 2).trim();
-                    }
-                    if (!TextUtils.isEmpty(deviceId)) {
-                        Intent mainActivity = new Intent(this, DeviceAddActivity.class);
-                        Bundle addInfo = getIntent().getBundleExtra("addInfo");
-                        addInfo.putString("deviceId", deviceId);
-                        mainActivity.putExtra("addInfo", addInfo);
-                        startActivity(mainActivity);
-                        return;
-                    }
-                }
-            }
-            CustomToast.makeText(this, "无效的设备ID号", R.drawable.ic_toast_warming);
-        } else if (requestCode == REQUEST_CODE_FOR_DSN_SCAN && resultCode == ScanActivity.RESULT_FOR_INPUT) {//扫码页面回退到手动输入页面
-            Intent mainActivity = new Intent(this, GatewayAddDsnInputActivity.class);
-            startActivityForResult(mainActivity, REQUEST_CODE_FOR_DSN_INPUT);
-        }
     }
 
     @Override
@@ -182,35 +110,7 @@ public class A2GatewayAddStatusActivity extends BaseMvpActivity<DeviceAddGuideVi
 
     @OnClick(R.id.bt)
     public void onClicked() {
-        if (checkBox.isChecked()) {
-            handleJump();
-        } else {
-            shakeButton();
-        }
-        String title_type = SharePreferenceUtils.getString(this, Constance.SP_SAAS, "1");
-        if (Constance.isNetworkDebug()) {//这个判断是dev，qa环境
-            if ("1".equalsIgnoreCase(title_type)) {
-                IoTSmart.setAuthCode("dev_saas");
-            } else {
-                IoTSmart.setAuthCode("dev_miya");
-            }
-            IoTSmart.init(MyApplication.getInstance(), new IoTSmart.InitConfig().setDebug(Constance.isNetworkDebug()));
-        } else {//这个是prod环境
-            if ("1".equalsIgnoreCase(title_type)) {
-                IoTSmart.setAuthCode("pord_saas");
-            } else {
-                IoTSmart.setAuthCode("china_production");
-            }
-        }
-        IoTSmart.init(MyApplication.getInstance(), new IoTSmart.InitConfig().setDebug(Constance.isNetworkDebug()));
+        handleJump();
     }
 
-    private void shakeButton() {
-        final AnimatorSet animSet = new AnimatorSet();
-        ObjectAnimator animX = ObjectAnimator.ofFloat(button, "translationX", 0, 10, 0, -10);
-        animX.setInterpolator(new CycleInterpolator(5));
-        animSet.setDuration(200);
-        animSet.play(animX);
-        animSet.start();
-    }
 }
