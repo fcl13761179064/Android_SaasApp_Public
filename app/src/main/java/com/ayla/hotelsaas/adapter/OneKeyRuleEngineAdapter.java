18f240f35ad53defd9c058f1fe7a0056d7bfe6a5
chunlei.fan <chunlei.fan@aylaasia.com>
2021-07-23 11:04:09 +0800

    1.本地和远端代码提交

--- app/src/main/java/com/ayla/hotelsaas/adapter/OneKeyRuleEngineAdapter.java
+++ app/src/main/java/com/ayla/hotelsaas/adapter/OneKeyRuleEngineAdapter.java
@@ -21,13 +21,17 @@ public class OneKeyRuleEngineAdapter extends BaseQuickAdapter<BaseSceneBean, Bas
     protected void convert(BaseViewHolder helper, BaseSceneBean ruleEngineBeans) {
         helper.setText(R.id.tv_device_name, ruleEngineBeans.getRuleName());
         helper.addOnClickListener(R.id.iv_more);
-
+        if (ruleEngineBeans.getSiteType() == 1) {
+            helper.setText(R.id.tv_local_remove, "本地");
+        } else {
+            helper.setText(R.id.tv_local_remove, "远端");
+        }
         if (ruleEngineBeans.getStatus() == 1) {
-            helper.setImageResource(R.id.iv_bg,R.drawable.bg_scene_onekey_enable);
+            helper.setImageResource(R.id.iv_bg, R.drawable.bg_scene_onekey_enable);
             helper.setVisible(R.id.ll_enable_state, true);
             helper.setVisible(R.id.tv_sub_1, false);
         } else {
-            helper.setImageResource(R.id.iv_bg,R.drawable.bg_scene_onekey_disable);
+            helper.setImageResource(R.id.iv_bg, R.drawable.bg_scene_onekey_disable);
             helper.setVisible(R.id.ll_enable_state, false);
             helper.setVisible(R.id.tv_sub_1, true);
         }
