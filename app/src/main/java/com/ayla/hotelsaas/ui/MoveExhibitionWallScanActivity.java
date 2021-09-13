b09ba87ddfa9ddd14131ee726818e69050a57387
chunlei.fan <chunlei.fan@aylaasia.com>
2021-09-13 17:38:47 +0800

    二维码

--- app/src/main/java/com/ayla/hotelsaas/ui/MoveExhibitionWallScanActivity.java
+++ app/src/main/java/com/ayla/hotelsaas/ui/MoveExhibitionWallScanActivity.java
@@ -165,9 +165,8 @@ public class MoveExhibitionWallScanActivity extends BaseMvpActivity<MoveWallView
             }.getType();
             ZxingMoveWallBean obj = GsonUtils.fromJson(result, type);
             if (obj != null) {
-                String id = obj.getId();
-                if (!TextUtils.isEmpty(id)) {
-                    mPresenter.getNetworkConfigGuide(id, obj);
+                if (!TextUtils.isEmpty(obj.getId())&&!TextUtils.isEmpty(obj.getName())&&!TextUtils.isEmpty(obj.getRoomId())) {
+                    mPresenter.getNetworkConfigGuide(obj.getId(), obj);
                 } else {
                     CustomAlarmDialog.newInstance().setTitle("信息错误")
                             .setContent(String.format("二维码信息错误，请检查信息正确后再扫描二维码"))
