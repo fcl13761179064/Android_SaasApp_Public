2e22f37ae601e7227d8d99046709d5266a451d9e
chunlei.fan <chunlei.fan@aylaasia.com>
2021-12-16 15:13:53 +0800

    删除一些垃圾代码

--- app/src/main/java/com/ayla/hotelsaas/page/ext/CommonExt.kt
+++ app/src/main/java/com/ayla/hotelsaas/page/ext/CommonExt.kt
@@ -2,43 +2,7 @@ package com.ayla.hotelsaas.page.ext
 
 import android.view.View
 import com.ayla.base.data.protocol.BaseResp
-import com.ayla.hotelsaas.page.rx.BaseSubscriber
-import com.ayla.hotelsaas.page.rx.ErrorConvert
-import com.ayla.hotelsaas.page.rx.RespConvert
 import com.blankj.utilcode.util.*
-import com.trello.rxlifecycle.android.ActivityEvent
-import com.trello.rxlifecycle.components.support.RxAppCompatActivity
-import rx.Observable
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
-
-
-/**
- * 网络请求（非MVP）
- */
-fun <T> Observable<T>.request(
-    lifecycleProvider: RxAppCompatActivity,
-    onSuccess: (value: T) -> Unit,
-    onFailure: (e: Throwable?) -> Unit = {}
-) {
-    this.subscribeOn(Schedulers.io())
-        .unsubscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .onErrorResumeNext(ErrorConvert())
-        .flatMap(RespConvert<T>())
-        .compose(lifecycleProvider.bindUntilEvent(ActivityEvent.DESTROY))
-        .subscribe(object : BaseSubscriber<T>() {
-            override fun onNext(t: T) {
-                super.onNext(t)
-                onSuccess.invoke(t)
-            }
-
-            override fun onError(e: Throwable?) {
-                super.onError(e)
-                onFailure.invoke(e)
-            }
-        })
-}
 
 
 /*
