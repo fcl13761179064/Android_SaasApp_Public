5fb58e9e402b8be7c7db8a4788aa32b9b1de4e64
chunlei.fan <chunlei.fan@aylaasia.com>
2021-12-20 10:05:41 +0800

    代码提交

--- app/src/main/java/com/ayla/hotelsaas/common/Keys.kt
+++ app/src/main/java/com/ayla/hotelsaas/common/Keys.kt
@@ -2,10 +2,9 @@ package com.ayla.hotelsaas.common
 
 object Keys {
 
+
     //const只能修饰在顶级属性，并且只在object对象属性中
     const val ID = "id"
     const val DATA = "data"
-     var RoomId = "roomId"
     const val MULTI_DEVICE_IDS = "multi_device_ids"
-    const val OEMMODEL = "cloud_model"
 }
